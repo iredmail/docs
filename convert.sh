4c91ae2402d579d8698dc9a41a3f56836a9e469b
Zhang Huangbin <Zhang Huangbin@localhost>
2015-07-12 13:51:26 +0800

    Directory structure change.

--- convert.sh
+++ convert.sh
@@ -3,17 +3,9 @@
 # Author: Zhang Huangbin <zhb _at_ iredmail.org>
 # Purpose: Convert markdown articles to HTML files.
 
-PWD="."
+ROOTDIR="$(pwd)"
 
-# Directory used to store converted html files.
-OUTPUT_DIR="${PWD}/html"
-
-# Markdown file used to store index of chapters/articles.
-INDEX_MD="${OUTPUT_DIR}/index.md"
-
-[ -d ${OUTPUT_DIR} ] || mkdir -p ${OUTPUT_DIR}
-
-CONVERTER="${PWD}/tools/markdown2html.py"
+CONVERTER="${ROOTDIR}/tools/markdown2html.py"
 CMD_CONVERT="python ${CONVERTER}"
 #CMD_CONVERT=":"
 CHANGED_FILES="$(hg st | grep '\.md$')"
@@ -29,6 +21,9 @@ strip_name_prefix()
     echo "${name}"
 }
 
+# Available translations
+all_languages='en_US zh_CN'
+
 # Chapter directories in specified order
 all_chapter_dirs="overview \
                   installation \
@@ -41,9 +36,6 @@ all_chapter_dirs="overview \
                   troubleshooting \
                   faq"
 
-# Initial index file.
-echo "All articles are written in Markdown format, you can download them for offline reading, contribute new articles or update existing ones in [BitBucket repository](https://bitbucket.org/zhb/docs.iredmail.org/)." > ${INDEX_MD}
-
 # Compile all Markdown files.
 if echo "$@" | grep -q -- '--all' &>/dev/null; then
     compile_all='YES'
@@ -52,79 +44,124 @@ fi
 article_counter=0
 echo -n "* Processing Markdown files: "
 
-# Get chapter info
-#   - chapter summary: _summary.md
-#   - article title: _title.md
-for chapter_dir in ${all_chapter_dirs}; do
-    # Get articles
-    all_chapter_articles="$(find ${chapter_dir} -depth 1 -type f -iname '[0-9a-z]*.md')"
-
-    # Output directory.
-    # Remove prefix '[number]-' in chapter directory name.
-    #chapter_dir_in_article="$(strip_name_prefix ${chapter_dir})"
-    #_output_chapter_dir="${OUTPUT_DIR}/${chapter_dir_in_article}"
-
-    # Get chapter title.
-    _title_md="${chapter_dir}/_title.md"
-    _summary_md="${chapter_dir}/_summary.md"
-
-    if [ -f ${_title_md} ]; then
-        # generate index info of chapter
-        _chapter_title="$(cat ${_title_md})"
-        echo -e "### ${_chapter_title}" >> ${INDEX_MD}
-
-        if [ -f ${_summary_md} ]; then
-            cat ${_summary_md} >> ${INDEX_MD}
-
-            # Insert an empty line to not mess up other formats like list.
-            echo '' >> ${INDEX_MD}
-        fi
+for lang in ${all_languages}; do
+    src_dir="${ROOTDIR}/docs/${lang}"
+    if [ ! -d ${src_dir} ]; then
+        echo "[SKIP] No translation for ${lang} (${src_dir})."
+        break
     fi
 
-    # Article info:
-    #   - title: first line (without '#') of markdown file
-    for article_file in ${all_chapter_articles}; do
-        article_counter="$((article_counter+1))"
+    cd ${src_dir}
 
-        article_file_basename="$(basename ${article_file})"
+    # Directory used to store converted html files.
+    OUTPUT_DIR="${ROOTDIR}/html"
+    if [ X"${lang}" != X'en_US' ]; then
+        OUTPUT_DIR="${ROOTDIR}/html/${lang}"
+    fi
 
-        article_html_file="$(strip_name_prefix ${article_file_basename})"
-        # Replace '.md' suffix by '.html'
-        article_html_file="$(echo ${article_html_file/%.md/.html})"
+    # Markdown file used to store index of chapters/articles.
+    INDEX_MD="${OUTPUT_DIR}/index.md"
 
-        hide_article_in_index='NO'
-        if echo "${article_file_basename}" | grep '^0-' &>/dev/null; then
-            hide_article_in_index='YES'
+    [ -d ${OUTPUT_DIR} ] || mkdir -p ${OUTPUT_DIR}
+
+    # Initial index file.
+    if [ -f ${src_dir}/_title.md ]; then
+        cat ${src_dir}/_title.md > ${INDEX_MD}
+    else
+        echo '' > ${INDEX_MD}
+    fi
+
+    # Get chapter info
+    #   - chapter summary: _summary.md
+    #   - article title: _title.md
+    for chapter_dir in ${all_chapter_dirs}; do
+        # Get articles
+        all_chapter_articles="$(find ${chapter_dir} -depth 1 -type f -iname '[0-9a-z]*.md')"
+
+        # Output directory.
+        # Remove prefix '[number]-' in chapter directory name.
+        #chapter_dir_in_article="$(strip_name_prefix ${chapter_dir})"
+        #_output_chapter_dir="${OUTPUT_DIR}/${chapter_dir_in_article}"
+
+        # Get chapter title.
+        _title_md="${chapter_dir}/_title.md"
+        _summary_md="${chapter_dir}/_summary.md"
+
+        if [ -f ${_title_md} ]; then
+            # generate index info of chapter
+            _chapter_title="$(cat ${_title_md})"
+            echo -e "### ${_chapter_title}" >> ${INDEX_MD}
+
+            if [ -f ${_summary_md} ]; then
+                cat ${_summary_md} >> ${INDEX_MD}
+
+                # Insert an empty line to not mess up other formats like list.
+                echo '' >> ${INDEX_MD}
+            fi
         fi
 
-        # Get title in markdown file: '# title'
-        _article_title="$(head -1 ${article_file} | awk -F'# ' '{print $2}')"
+        # Used to prettier print
+        break_line='NO'
 
-        if [ X"${hide_article_in_index}" == X'NO' ]; then
-            echo "* [${_article_title}](${article_html_file})" >> ${INDEX_MD}
-        fi
+        # Article info:
+        #   - title: first line (without '#') of markdown file
+        for article_file in ${all_chapter_articles}; do
+            article_counter="$((article_counter+1))"
+            article_file_basename="$(basename ${article_file})"
+            article_html_file="$(strip_name_prefix ${article_file_basename})"
+            # Replace '.md' suffix by '.html'
+            article_html_file="$(echo ${article_html_file/%.md/.html})"
 
-        # Convert modified file
-        echo ${CHANGED_FILES} | grep ${article_file} &> /dev/null
-        compile_this_file="$?"
+            hide_article_in_index='NO'
+            if echo "${article_file_basename}" | grep '^0-' &>/dev/null; then
+                hide_article_in_index='YES'
+            fi
 
-        if [ X"${compile_this_file}" == X'0' -o X"${compile_all}" == X'YES' ]; then
-            echo -e "\n* Converting (#${article_counter}): ${article_file}"
-            ${CMD_CONVERT} ${article_file} ${OUTPUT_DIR} \
-                output_filename="${article_html_file}" \
-                title="${_article_title}" \
-                add_index_link='yes'
-        else
-            echo -n '.'
+            # Get title in markdown file: '# title'
+            _article_title="$(head -1 ${article_file} | awk -F'# ' '{print $2}')"
+
+            if [ X"${hide_article_in_index}" == X'NO' ]; then
+                echo "* [${_article_title}](${article_html_file})" >> ${INDEX_MD}
+            fi
+
+            # Convert modified file
+            echo ${CHANGED_FILES} | grep ${article_file} &> /dev/null
+            compile_this_file="$?"
+
+            if [ X"${compile_this_file}" == X'0' -o X"${compile_all}" == X'YES' ]; then
+                if [ X"${break_line}" == X'YES' ]; then
+                    echo -en "* Converting (#${article_counter}): ${lang}/${article_file}"
+                else
+                    echo -en "\n* Converting (#${article_counter}): ${lang}/${article_file}"
+                fi
+
+                # Convert
+                ${CMD_CONVERT} ${article_file} \
+                               ${OUTPUT_DIR} \
+                               output_filename="${article_html_file}" \
+                               title="${_article_title}" \
+                               add_index_link='yes'
+
+                if [ X"$?" == X'0' ]; then
+                    echo -e ' [DONE]'
+                else
+                    echo -e ' <<< FAILED >>>'
+                fi
+
+                break_line='YES'
+            else
+                echo -n '.'
+                break_line='NO'
+            fi
+        done
+
+        # Append addition links at the chapter bottom on index page.
+        _links_md="${chapter_dir}/_links.md"
+
+        if [ -f ${_links_md} ]; then
+            cat ${_links_md} >> ${INDEX_MD}
         fi
     done
-
-    # Append addition links at the chapter bottom on index page.
-    _links_md="${chapter_dir}/_links.md"
-
-    if [ -f ${_links_md} ]; then
-        cat ${_links_md} >> ${INDEX_MD}
-    fi
 done
 
 echo ''
@@ -141,9 +178,9 @@ if echo "$@" | grep -q -- '--sync-local'; then
     # Copy to local hg repo of http://www.iredmail.org/docs/
     echo "* Syncing converted HTML files."
     rm -rf ../web/docs/*
-    cp -rf html/* ../web/docs/
+    cp -rf ${ROOTDIR}/html/* ${ROOTDIR}/../web/docs/
 
     # Copy to iredmail.com/docs/
     rm -rf /Volumes/STORAGE/Dropbox/Backup/iredmail.com/docs/*
-    cp -rf html/* /Volumes/STORAGE/Dropbox/Backup/iredmail.com/docs/
+    cp -rf ${ROOTDIR}/html/* /Volumes/STORAGE/Dropbox/Backup/iredmail.com/docs/
 fi
